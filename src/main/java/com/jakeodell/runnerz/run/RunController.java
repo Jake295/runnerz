dc00c3118157728909cd7ed60a4592b16d151589
Jake O'Dell <jakeodell66@gmail.com>
2024-05-20 20:07:01 -0700

    RunRepository added

--- src/main/java/com/jakeodell/runnerz/run/RunController.java
+++ src/main/java/com/jakeodell/runnerz/run/RunController.java
@@ -4,11 +4,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class RunController {
 
-    @GetMapping("/hello")
-    String home() {
-        return "Hello, Runnerz!";
+    private final RunRepository runRepository;
+
+    //Access RunRepository class already initialized by Spring. Found by @Repository
+    public RunController(RunRepository runRepository) {
+        this.runRepository = runRepository;
+    }
+
+    @GetMapping("/api/runs")
+    List<Run> findAll() {
+        return runRepository.findAll();
     }
 }
---
+++ src/main/java/com/jakeodell/runnerz/run/RunRepository.java
@@ -0,0 +1,36 @@
+package com.jakeodell.runnerz.run;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RunRepository {
+
+    private List<Run> runs = new ArrayList<>();
+
+    List<Run> finalAll() {
+        return runs;
+    }
+
+    @PostConstruct
+    private void init() {
+        runs.add(new Run(1,
+                "Monday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
+                3,
+                Location.INDOOR));
+
+        runs.add(new Run(2,
+                "Wednesday Evening Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
+                6,
+                Location.INDOOR));
+    }
+}
